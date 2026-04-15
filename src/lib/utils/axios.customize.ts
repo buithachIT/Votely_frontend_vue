3a4a9565e08afe2c8e5f99d74c9de66fe5c7c214
Bui Cong Thach <buithach311@Nass-Mac.local>
2026-04-15 16:34:22 +0700

    delete .prettierrc

--- src/lib/utils/axios.customize.ts
+++ src/lib/utils/axios.customize.ts
@@ -1,13 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api",
   timeout: 10000,
 });
 
 instance.interceptors.request.use(
   function (config) {
-    const token = localStorage.getItem('access_token');
+    const token = localStorage.getItem("access_token");
 
     if (token && config.headers) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -17,7 +17,7 @@ instance.interceptors.request.use(
   },
   function (error) {
     return Promise.reject(error);
-  }
+  },
 );
 instance.interceptors.response.use(
   (response) => {
@@ -35,9 +35,9 @@ instance.interceptors.response.use(
 
     return Promise.reject({
       success: false,
-      message: error.message || 'Lỗi kết nối Server',
+      message: error.message || "Lỗi kết nối Server",
       errors: null,
     });
-  }
+  },
 );
 export default instance;
